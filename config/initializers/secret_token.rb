254f7419f9bae68452ebc0f3feab299261046f61
Dan Croak <dcroak@thoughtbot.com>
2010-10-08 15:31:22 -0400

    rails 3 via suspenders gem, modified for postgres. install instructions in doc/README_FOR_APP.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reliefhub::Application.config.secret_token = 'd51c2f9e34e047556653f930fa2ed3340f0ae49c04ba31e41c1050ecaa2d688ddcc64f1d56bfeaa8bda597d99d45396e1e83bb61315278c144b43ff43d89ec24'
