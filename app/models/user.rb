2e3da67b3b46f9f1745cb81b85f6ec4f9ec325e4
Dan Croak <dcroak@thoughtbot.com>
2010-12-04 17:25:54 -0500

    adding test coverage, twitter link

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,21 @@ class User < ActiveRecord::Base
          :rpx_connectable
 
   attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me
-  
+
   has_many :donations
-  has_many :projects, :through=>:donations
+  has_many :projects, :through => :donations
 
   validates_presence_of :email
   validates_presence_of :first_name
   validates_presence_of :last_name
-  
-  def projects_i_have_contributed_to
+
+  def unique_projects
     projects.uniq
- end
-  def update_with_password(params={}) 
-     if params[:password].blank? 
-       params.delete(:password) 
-       params.delete(:password_confirmation) if params[:password_confirmation].blank? 
-     end 
-     update_attributes(params) 
-   end
+  end
+
+  def update_with_password(params={})
+    params.delete(:password)              if params[:password].blank?
+    params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    update_attributes(params)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,23 @@ require 'spec_helper'
 
 describe User do
   it { should have_many(:donations) }
-  
+
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:first_name) }
-  it { should validate_presence_of(:last_name) }  
+  it { should validate_presence_of(:last_name) }
 end
 
+describe User, "#unique_projects" do
+  subject       { Factory(:user)    }
+  let(:project) { Factory(:project) }
+
+  before do
+    2.times do
+      Factory(:donation, :user => subject, :project => project)
+    end
+  end
+
+  it "returns only unique projects" do
+    subject.unique_projects.should == [project]
+  end
+end
